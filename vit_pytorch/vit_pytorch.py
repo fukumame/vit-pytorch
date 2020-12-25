05659b51362f709a053af09869677f4cba13a0bf
fukumame <tanuma0115@gmail.com>
2020-12-25 20:45:24 +0900

    テスト実行用のコードを追加

--- vit_pytorch/vit_pytorch.py
+++ vit_pytorch/vit_pytorch.py
@@ -153,3 +153,22 @@ class ViT(nn.Module):
 
         x = self.to_latent(x)
         return self.mlp_head(x)  # x: (batch) x (class数)
+
+
+if __name__ == '__main__':
+    v = ViT(
+        image_size=256,
+        patch_size=32,
+        num_classes=1000,
+        dim=1024,
+        depth=6,
+        heads=16,
+        mlp_dim=2048,
+        dropout=0.1,
+        emb_dropout=0.1
+    )
+
+    img = torch.randn(3, 3, 256, 256)
+    mask = torch.ones(1, 8, 8).bool()  # optional mask, designating which patch to attend to
+
+    preds = v(img, mask=mask)  # (1, 1000)
